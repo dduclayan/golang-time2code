9e128d55dd670893249b24255ad1ccdb416e62ae
dduclayan <dduclayan@gmail.com>
2021-12-14 02:30:21 -0600

    dedupe_file

--- dedupeFile/main.go
+++ dedupeFile/main.go
@@ -1,21 +1,68 @@
+/*
+Given a file input with multiple lines and some duplicates.
+
+Output a new file with duplicates removed.
+*/
+
 package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"sort"
 )
 
-func dedupeFile(filePath string) {
+func check(e error) {
+	if e != nil {
+		log.Panic(e)
+	}
+}
+
+func dedupeFile(filePath string) []string {
 	file, err := os.Open(filePath)
-	if err != nil {
-		log.Panic(err)
+	check(err)
+	defer file.Close()
+
+	lineCount := make(map[string]bool)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if _, ok := lineCount[line]; ok {
+		} else {
+			lineCount[line] = true
+		}
 	}
 
-	scanner := bufio.NewScanner(file)
+	var dedupedStrings []string
+	for k := range lineCount {
+		dedupedStrings = append(dedupedStrings, k)
+	}
+	sort.Strings(dedupedStrings)
+	fmt.Println(dedupedStrings)
+
+	return dedupedStrings
+}
+
+func writeStrings(s []string) {
+	outfile := "D:\\Documents\\go_workspace\\src\\learningGoAgain\\dedupeFile\\file2.txt"
+	file, err := os.Create(outfile)
+	check(err)
+	defer file.Close()
+
+	fmt.Println("Logging to file...")
+	for _, v := range s {
+		_, err := file.Write([]byte(v + "\n"))
+		if err != nil {
+			return
+		}
+	}
 }
 
 func main() {
-	file := "D:\\Documents\\go_workspace\\src\\learningGoAgain\\dedupe_file\\logFile.txt"
-	dedupeFile(file)
+	file := "D:\\Documents\\go_workspace\\src\\learningGoAgain\\dedupeFile\\logFile.txt"
+	x := dedupeFile(file)
+	writeStrings(x)
 }
